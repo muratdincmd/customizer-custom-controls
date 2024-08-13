6647db2b822608c4ff73e89ee9ddcfb65e9b17af
Murat Dinc <muratdinc.md@gmail.com>
2024-08-13 15:48:03 +0300

    New class added, css improved, icons updated.

--- inc/custom-controls.php
+++ inc/custom-controls.php
@@ -1386,14 +1386,14 @@ if ( class_exists( 'WP_Customize_Control' ) ) {
 			return skyrocket_in_range( $number, $min, $max );
 		}
 	}
-   /**
+	/**
     * Laura Sortable Checkbox Button Custom Control
     *
     * @author Anthony Hortin <http://maddisondesigns.com>
     * @license http://www.gnu.org/licenses/gpl-2.0.html
     * @link https://github.com/maddisondesigns
     */
-   class Laura_Checkbox_Button_Custom_Control extends Skyrocket_Custom_Control {
+	class Laura_Checkbox_Button_Custom_Control extends Skyrocket_Custom_Control {
 		/**
 		 * The type of control being rendered
 		 */
@@ -1428,5 +1428,50 @@ if ( class_exists( 'WP_Customize_Control' ) ) {
 			</div>
 		<?php
 		}
-   }
+	}
+	/**
+	 * Text Radio Button Custom Control for Laura
+    *
+    * @author Anthony Hortin <http://maddisondesigns.com>
+    * @license http://www.gnu.org/licenses/gpl-2.0.html
+    * @link https://github.com/maddisondesigns
+	 */
+	class Laura_Text_Radio_Button_Custom_Control extends WP_Customize_Control {
+		/**
+		 * The type of control being rendered
+		 */
+		public $type = 'text_radio_button';
+
+		/**
+		 * Enqueue our scripts and styles
+		 */
+		public function enqueue() {
+			wp_enqueue_style( 'laura-custom-controls-css', get_template_directory_uri() . '/inc/customizer-custom-controls/css/customizer.css', array(), '1.0', 'all' );
+		}
+
+		/**
+		 * Render the control in the customizer
+		 */
+		public function render_content() {
+			?>
+			<div class="text_radio_button_control">
+				<?php if( !empty( $this->label ) ) { ?>
+					<span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
+				<?php } ?>
+				<?php if( !empty( $this->description ) ) { ?>
+					<span class="customize-control-description"><?php echo esc_html( $this->description ); ?></span>
+				<?php } ?>
+
+				<div class="radio-buttons">
+					<?php foreach ( $this->choices as $key => $value ) { ?>
+						<label class="radio-button-label">
+							<input type="radio" name="<?php echo esc_attr( $this->id ); ?>" value="<?php echo esc_attr( $key ); ?>" <?php $this->link(); ?> <?php checked( esc_attr( $key ), $this->value() ); ?>/>
+							<span><?php echo $value; // HTML'i ekrana basmak için esc_html kullanmadık ?></span>
+						</label>
+					<?php } ?>
+				</div>
+			</div>
+			<?php
+		}
+	}
 }
